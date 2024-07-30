ab2923a8352fc5e7023b03a57b82793b5ac7953c
abxshai <147988199+abxshai@users.noreply.github.com>
2024-07-31 05:21:49 +0530

    Update file.py

--- file.py
+++ file.py
@@ -106,7 +106,7 @@ css = """
 </style>
 <video autoplay muted loop id="myVideo">
     <source src="https://static.streamlit.io/examples/star.mp4" type="video/mp4">
-    Your browser does not support HTML5 video.
+  
 </video>
 """
 
